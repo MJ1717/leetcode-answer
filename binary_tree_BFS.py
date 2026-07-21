5af6dec7025e31979e92ec62d300a197dfa5ed67
MJ-valital <bananahamo77@gmail.com>
2026-07-21 19:54:29 -0400

    .

---
+++ binary_tree_BFS.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+
+        #1. if root is empty, return []
+        #2. we want to see the most right node, in each level
+
+        if (root == None):
+            return []
+
+        queue = [root]
+        result = []
+    
+        while (queue):
+            for _ in range(len(queue)):
+                current = queue.pop(0)
+                
+                if (current.left != None):
+                    queue.append(current.left)
+                
+                if (current.right != None):
+                    queue.append(current.right)
+            
+            result.append(current.val)
+
+        return result
+
+
+
+            
+
+
+
+
+        
