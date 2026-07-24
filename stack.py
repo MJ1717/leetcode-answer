b3bc6621d5f117a1f50e4da0589e90c6280fda33
MJ-valital <bananahamo77@gmail.com>
2026-07-23 23:26:27 -0400

    .

--- stack.py
+++ stack.py
@@ -19,4 +19,59 @@ class Solution(object):
 
         return "".join(reversed(result))
 
+
+from collections import deque
+
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+
+        def is_collision(old, new):
+            if (new * old > 0):
+                return False
+            if (old < 0 and new > 0):
+                return False
+            if (old > 0 and new < 0):
+                return True
+
+        result = []
+
+        for i in range(len(asteroids)):
+            current = asteroids[i]
+
+            if (len(result) == 0):
+                result.append(current)
+                continue
+
+            result.append(current)
+
+            new = result[len(result) - 1]
+            old = result[len(result) - 2]
+            while (is_collision(old, new)):
+                if (abs(new) > abs(old)):
+                    result.pop()
+                    result.pop()
+                    result.append(new)
+                elif (abs(new) < abs(old)):
+                    result.pop()
+                    break
+                else:
+                    result.pop()
+                    result.pop()
+                    break
+                new = result[len(result) - 1]
+                old = result[len(result) - 2]
+
+        return result
+
+
+
+
+
+            
+
+
         
