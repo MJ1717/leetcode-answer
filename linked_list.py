e6c8d69c34613d8e993ea3fda3d0aa5ffadbaeeb
MJ-valital <bananahamo77@gmail.com>
2026-07-30 19:21:28 -0400

    aa

--- linked_list.py
+++ linked_list.py
@@ -68,6 +68,50 @@ class Solution(object):
 
             current = next_element
 
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def pairSum(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: int
+        """
+
+        # edge case
+        if (head is None):
+            return 0
+
+        array = []
+        current = head
+        array.append(current.val)
+
+        # update array
+        while (current.next is not None):
+            current = current.next
+            array.append(current.val)
+
+        front = 0
+        back = len(array) - 1
+        maxx = -1
+
+        while (front < back):
+            current_sum = array[front] + array[back]
+            maxx = max(maxx, current_sum)
+
+            front += 1
+            back -= 1
+            
+        return maxx
+
+        
+
+        
+
   
 
 
