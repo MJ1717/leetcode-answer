539da14cc70b57c0b776677a223b2821001fd817
MJ-valital <bananahamo77@gmail.com>
2026-07-29 21:13:29 -0400

    a

--- linked_list.py
+++ linked_list.py
@@ -27,3 +27,48 @@ class Solution(object):
         array[modify_index].next = array[modify_index + 2]
 
         return head
+
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        # edge case
+        if (head is None):
+            return None
+        if (head.next is None):
+            return head
+
+        prev = None
+        current = head
+        next_element = head.next
+        
+        while (True):
+            # save next element
+            next_element = current.next
+
+            # save current
+            tmp_prev = current
+
+            # update
+            current.next = prev
+            prev = tmp_prev     
+
+            if (next_element is None):
+                return current
+
+            current = next_element
+
+  
+
+
+        
