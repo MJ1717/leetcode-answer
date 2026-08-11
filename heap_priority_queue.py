b97c0da747acc96eb73315d0b4ff49ad3dcf7b2d
MJ-valital <bananahamo77@gmail.com>
2026-08-11 15:18:57 -0400

    a

--- heap_priority_queue.py
+++ heap_priority_queue.py
@@ -9,3 +9,35 @@ class Solution:
                 return heapq.heappop_max(nums)
 
             heapq.heappop_max(nums)
+
+
+import heapq
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.current = 1
+        self.heap = []
+        
+
+    def popSmallest(self) -> int:
+        if (len(self.heap) != 0):
+            return heapq.heappop(self.heap)
+        
+        else:
+            res = self.current
+            self.current += 1
+            return res
+        
+
+    def addBack(self, num: int) -> None:
+        if (num < self.current):
+            if (num not in self.heap):
+                heapq.heappush(self.heap, num)
+        
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
