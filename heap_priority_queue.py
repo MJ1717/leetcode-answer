339f69981c545c4e41f9095702ef86363f279352
MJ-valital <bananahamo77@gmail.com>
2026-08-12 18:14:06 -0400

    a

--- heap_priority_queue.py
+++ heap_priority_queue.py
@@ -41,3 +41,46 @@ class SmallestInfiniteSet:
 # obj = SmallestInfiniteSet()
 # param_1 = obj.popSmallest()
 # obj.addBack(num)
+
+
+
+import heapq
+
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        
+        pairs = []
+        for i in range(len(nums1)):
+            pairs.append([nums1[i], nums2[i]])
+
+        def get_second_element(x):
+            return x[1]
+
+        pairs.sort(key = get_second_element, reverse = True)
+
+        current_sum = 0
+        heap = []
+
+        for i in range(k):
+            current_sum += pairs[i][0]
+            heapq.heappush(heap, pairs[i][0])
+
+        maxx = current_sum * pairs[k-1][1]
+
+        right = k
+
+        while (right < len(pairs)):
+            right_nums1 = pairs[right][0]
+            right_nums2 = pairs[right][1]
+
+            prev_smallest = heapq.heappop(heap)
+
+            heapq.heappush(heap, right_nums1)
+            current_sum = current_sum - prev_smallest + right_nums1
+
+            maxx = max(maxx, current_sum * right_nums2)
+
+            right += 1
+
+        return maxx
+
