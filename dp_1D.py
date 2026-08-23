c7ab8540011180fa39ea78d92559083a04d528da
MJ-valital <bananahamo77@gmail.com>
2026-08-22 20:37:27 -0400

    A

--- dp_1D.py
+++ dp_1D.py
@@ -37,3 +37,27 @@ class Solution:
             dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
 
         return min(dp[len(cost) - 1], dp[len(cost) - 2])
+
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        # edge case
+        if (len(nums) == 0):
+            return 0
+        if (len(nums) == 1):
+            return nums[0]
+        if (len(nums) == 2):
+            return max(nums[0], nums[1])
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+
+        return max(dp[-1], dp[-2])      
+
+        
