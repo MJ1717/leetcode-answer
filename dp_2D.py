2dbf6d3f1737c90ddecd2be60287a4dc65746756
MJ-valital <bananahamo77@gmail.com>
2026-08-26 23:47:41 -0400

    a

--- dp_2D.py
+++ dp_2D.py
@@ -25,19 +25,26 @@ class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n = len(text1)
         m = len(text2)
-        prev = 0
 
         dp = [ [0] * (m + 1) for _ in range(n + 1) ]
 
         for i in range(1, n + 1):
             for j in range(1, m + 1):
 
+                # when we have same char, we get the each best
+                # optimal solution from (one char back) each
                 if text1[i - 1] == text2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
 
+                # same as above, since we cannnot add one more, we
+                # want to keep the best ideal solution for current ij.
+                # and best ideal solution will be based on
+                # which on is better? when we didnt see first text one less
+                # or second text one less?
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
                 
 
         return dp[-1][-1]
+
         
