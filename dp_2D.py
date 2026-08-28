a3c2565f84d73e0e2f5463e27e5c8a627aeaa3ac
MJ-valital <bananahamo77@gmail.com>
2026-08-28 01:30:22 -0400

    a

--- dp_2D.py
+++ dp_2D.py
@@ -47,4 +47,17 @@ class Solution:
 
         return dp[-1][-1]
 
-        
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+
+        hold = [0] * len(prices)
+        cash = [0] * len(prices)
+        hold[0] = - prices[0]
+
+        for i in range(1, len(prices)):
+            hold[i] = max(hold[i-1], cash[i-1] - prices[i])
+            cash[i] = max(cash[i-1], hold[i-1] + prices[i] - fee)
+
+        return cash[-1]
+
