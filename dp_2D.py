f98dfac460cadf893c42509d92400f69eabfa932
MJ-valital <bananahamo77@gmail.com>
2026-08-29 01:51:41 -0400

    a

--- dp_2D.py
+++ dp_2D.py
@@ -61,3 +61,36 @@ class Solution:
 
         return cash[-1]
 
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        # word1 -> empty string
+        for i in range(n + 1):
+            dp[i][0] = i
+
+        # empty string -> word2
+        for j in range(m + 1):
+            dp[0][j] = j
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+
+                # same character
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+
+                # different character
+                else:
+                    delete = dp[i - 1][j]
+                    insert = dp[i][j - 1]
+                    replace = dp[i - 1][j - 1]
+
+                    dp[i][j] = 1 + min(delete, insert, replace)
+
+        return dp[n][m]
+
