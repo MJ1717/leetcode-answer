f60508788cede57b266437d9aa0570ce6e78941c
MJ-valital <bananahamo77@gmail.com>
2026-08-31 17:36:51 -0400

    a

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -6,3 +6,15 @@ class Solution:
             result[i] = result[i // 2] + (i % 2)
 
         return result
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        result = 0
+
+        for val in nums:
+            result = result ^ val
+
+        return result
+            
