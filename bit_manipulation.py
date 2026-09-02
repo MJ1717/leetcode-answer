1b70954152632582dfb54daf4ce97a96c845b46d
MJ-valital <bananahamo77@gmail.com>
2026-09-02 01:15:34 -0400

    a

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -18,3 +18,43 @@ class Solution:
 
         return result
             
+
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+
+        def put_zero(string, num):
+            current_len = len(string)
+
+            while (current_len < num):
+                string = "0" + string
+                current_len += 1
+
+            return string
+
+        a_bin = bin(a)[2:]
+        b_bin = bin(b)[2:]
+        c_bin = bin(c)[2:]
+
+        max_length = max(len(a_bin), len(b_bin), len(c_bin))
+
+        a_bin = put_zero(a_bin, max_length)
+        b_bin = put_zero(b_bin, max_length)
+        c_bin = put_zero(c_bin, max_length)
+
+        count = 0
+
+        for i in range(len(a_bin)):
+            if (c_bin[i] == '1'):
+                if (a_bin[i] != '1' and b_bin[i] != '1'):
+                    count += 1
+            
+            elif (c_bin[i] == '0'):
+                if (a_bin[i] == '1'):
+                    count += 1
+                if (b_bin[i] == '1'):
+                    count += 1
+
+        return count
+            
+
+        
