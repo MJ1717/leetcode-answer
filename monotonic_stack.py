85d07bdefcfa2976255e2357e20a262ecf386912
MJ-valital <bananahamo77@gmail.com>
2026-09-08 20:54:34 -0400

    a

--- monotonic_stack.py
+++ monotonic_stack.py
@@ -19,6 +19,39 @@ class Solution:
 
         return result
 
+
+
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+        self.day = 0
+        
+
+    def next(self, price: int) -> int:
+        self.day += 1
+
+        while (self.stack and price >= self.stack[-1][0]):
+            self.stack.pop()
+        
+        # if stack is empty
+        if (not self.stack):
+            span = self.day
+    
+        # there is element
+        else:
+            last_day = self.stack[-1][1]
+            span = self.day - last_day
+
+        self.stack.append([price, self.day])
+
+        return span
+        
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
             
 
         
