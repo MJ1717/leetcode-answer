3e1f4f51d4c6a2074f12cc5ec79652e6d21343e5
MJ-valital <bananahamo77@gmail.com>
2026-09-10 16:20:55 -0400

    a

--- divide_and_conquer.py
+++ divide_and_conquer.py
@@ -28,3 +28,83 @@ class Solution:
 
         return build(0, len(nums) - 1)
 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        # # edge case
+        # if (head == None):
+        #     return None
+
+        # tmp = []
+
+        # current = head
+
+        # while (current != None):
+        #     tmp.append([current, current.val])
+        #     current = current.next
+
+        # tmp.sort(key=lambda i: i[1])
+
+        # for i in range(len(tmp) - 1):
+        #     node, val = tmp[i]
+
+        #     node.next = tmp[i+1][0]
+            
+        # tmp[-1][0].next = None
+
+        # return tmp[0][0]
+
+        def sort(head):
+
+            if (head is None or head.next is None):
+                return head
+
+            slow, fast = head, head.next
+
+            while(fast and fast.next):
+                slow = slow.next
+                fast = fast.next.next
+
+            second_half = slow.next
+            slow.next = None
+
+            first = sort(head)
+            second = sort(second_half)
+
+            return merge(first, second)
+
+        def merge(node1, node2):
+
+            dummy = ListNode(0)
+            current = dummy
+            
+            while (node1 and node2):
+                if (node1.val < node2.val):
+                    current.next = node1
+                    node1 = node1.next
+                else:
+                    current.next = node2
+                    node2 = node2.next
+
+                current = current.next
+
+            # left over
+            if (node1):
+                current.next = node1
+
+            if (node2):
+                current.next = node2
+
+            return dummy.next
+
+        return sort(head)
+
+            
+
+            
